93664935e1511d19390202170d574eedb17ec6e1
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-09-07 20:05:58 +0200

    hello

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,26 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        '-0.35': '-0.035em',
+      },
+      lineHeight: {
+        '135': '1.355',
+      },
+      colors: {
+        thrasker: {
+          header: "#242424",
+          text: "#BCBCBC",
+          secondPage: "#6C6C6C",
+          textDarker: "#DDD",
+          textWhitePage: "#333",
+        },
+      },
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
